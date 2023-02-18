2bed5328db21a7885997ff6d14bbf059abcd466a
Julian David <julianfullstackdev@gmail.com>
2023-02-18 15:50:56 -0500

    add title and paragraph

--- app/page.js
+++ app/page.js
@@ -1,4 +1,17 @@
 export default function Page() {
-    return <h1>Hello, Next.js!, soy un conflicto</h1>;
-  }
-
+  return (
+    <div>
+      <h1>Page</h1>
+      <p
+        style={{
+          fontSize: "20px",
+          color: "green",
+          marginTop: "1rem",
+          fontFamily: "Verdana",
+        }}
+      >
+        This is our first paragraph
+      </p>
+    </div>
+  );
+}

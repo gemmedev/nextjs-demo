5fc2b7234c6c88cdf193c06f98bfbbed637eb799
Julian David <julianfullstackdev@gmail.com>
2023-03-04 09:01:58 -0500

    change home component

--- components/home/Home.jsx
+++ components/home/Home.jsx
@@ -1,7 +1,8 @@
 export default function Home (){
     return (
         <div>
-            <h1>Home</h1>
+            <h1>Home with changes</h1>
+            <p className="text-black">This is a test</p>
         </div>
     )
 }

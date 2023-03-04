7024c880e20eab31d59969d15355b5935342e81c
Julian David <julianfullstackdev@gmail.com>
2023-03-04 09:07:05 -0500

    change a text

--- components/home/Home.jsx
+++ components/home/Home.jsx
@@ -2,7 +2,7 @@ export default function Home (){
     return (
         <div>
             <h1>Home with changes</h1>
-            <p className="text-black">This is a test</p>
+            <p className="text-black">This is a test, sorry, I am the main, this is a conflict</p>
         </div>
     )
 }
